c7407f2a93ea52c59f1e30e80d84a624eee695c3
panroger <panroger66@gmail.com>
2022-10-21 15:56:48 +0800

    棋盤創建OK

---
+++ 九宮格/Assets/GameManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject Grid;
+    public Transform Board;
+    public Color gridLight;
+    public Color gridDark;  //default color
+    public Transform empty;
+
+    void Start()
+    {
+        //CreateBG();
+    }
+
+    //create board and grid
+    private void CreateBG(){
+        var p = Instantiate(empty, Vector3.zero, Quaternion.identity);
+        Instantiate(Board, new Vector3(1, 1, 0), Quaternion.identity, p);
+
+        for(int i = 0; i < 3; ++i){
+            for(int j = 0; j < 3; ++j){
+                var g = Instantiate(Grid, new Vector3(i, j, 0), Quaternion.identity, p);
+                g.name = $"grid {i}, {j}";
+
+                if((i+j) % 2 != 0)
+                    g.GetComponent<SpriteRenderer>().color = gridLight;
+            }
+        }
+    }
+}
